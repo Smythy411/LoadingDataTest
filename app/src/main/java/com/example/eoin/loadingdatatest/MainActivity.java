7c1b9611f664147b1ba826e400de7ab3429cf892
Eoin Smyth <c14427202@mydit.ie>
2017-11-02 11:18:50 +0000

    After a lot of messing around with the libraries, There is now a simple route between the two points.

--- app/src/main/java/com/example/eoin/loadingdatatest/MainActivity.java
+++ app/src/main/java/com/example/eoin/loadingdatatest/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.eoin.loadingdatatest;
 import android.app.Activity;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.osmdroid.api.IMapController;
-import org.osmdroid.bonuspack.routing.OSRMRoadManager;
+import org.osmdroid.bonuspack.routing.MapQuestRoadManager;
 import org.osmdroid.bonuspack.routing.Road;
 import org.osmdroid.bonuspack.routing.RoadManager;
 import org.osmdroid.config.Configuration;
@@ -20,6 +22,10 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends Activity {
+
+    //Map view
+    MapView map;
+
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Context ctx = getApplicationContext();
@@ -27,8 +33,7 @@ public class MainActivity extends Activity {
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
         setContentView(R.layout.activity_main);
 
-        //Map view
-        MapView map = (MapView) findViewById(R.id.map);
+        map = (MapView) findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
 
         //Enables zoom
@@ -38,7 +43,7 @@ public class MainActivity extends Activity {
         //Sets the inital zoom level and starting location
         IMapController mapController = map.getController();
         mapController.setZoom(17);
-        GeoPoint myAddress = new GeoPoint(53.279635, -6.346731);
+        GeoPoint myAddress = new GeoPoint(53.2795432, -6.3469185);
         mapController.setCenter(myAddress);
 
         //Simple marker for the starting node of the route
@@ -62,18 +67,45 @@ public class MainActivity extends Activity {
         map.getOverlays().add(endMarker);
         endMarker.setTitle("End point");
 
-        /*
-        //Constructing the route
-        RoadManager roadManager = new OSRMRoadManager(this);
-        Road road = roadManager.getRoad(waypoints);
-        Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
+        map.invalidate();
 
-        map.getOverlays().add(roadOverlay);
-        */
+        //In order to execute the network operations, this task can not be done on the main thread.
+        //The waypoints are passed to this thread in order to map a route.
+        new UpdateRoadTask().execute(waypoints);
 
         TextView textViewCurrentLocation = (TextView) findViewById(R.id.textViewCurrentLocation);
 
-    }
+    }//End OnCreate()
+
+    /**
+     * Async task to get the road in a separate thread.
+     * Credit to https://stackoverflow.com/questions/21213224/roadmanager-for-osmdroid-error
+     */
+    private class UpdateRoadTask extends AsyncTask<Object, Void, Road> {
+
+        protected Road doInBackground(Object... params) {
+            @SuppressWarnings("unchecked")
+            ArrayList<GeoPoint> waypoints = (ArrayList<GeoPoint>)params[0];
+            RoadManager roadManager = new MapQuestRoadManager("4EVl90eNYFzKk3Cp0RMvMLsyzC1PXYL1");
+
+
+            return roadManager.getRoad(waypoints);
+        }//End doInBackground()
+        @Override
+        protected void onPostExecute(Road result) {
+            Road road = result;
+            // showing distance and duration of the road
+            Toast.makeText(MainActivity.this, "distance="+road.mLength, Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "duration="+road.mDuration, Toast.LENGTH_SHORT).show();
+
+            if(road.mStatus != Road.STATUS_OK)
+                Toast.makeText(MainActivity.this, "Error when loading the road - status="+road.mStatus, Toast.LENGTH_SHORT).show();
+            Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
+
+            map.getOverlays().add(roadOverlay);
+            map.invalidate();
+        }//End onPostExecute()
+    }//End UpdateRoadTask()
 
     public void onResume(){
         super.onResume();
@@ -82,5 +114,5 @@ public class MainActivity extends Activity {
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Configuration.getInstance().save(this, prefs);
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
-    }
-}
+    }//End onResume()
+}//End MainActivity
